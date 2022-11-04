7d8e3917e8966ecc7e5d02f3d6a2d9e471a54255
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-04 10:41:56 -0300

    make login post

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import axios from "axios";
 import express from "express";
 import cors from "cors";
 
+const users = [];
+
+const tweets = [];
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -9,6 +13,22 @@ app.listen(5000);
 
 app.get("/here", (req, res) => {
   res.send("Hello!!");
+});
+
+app.post("/sign-up", (req, res) => {
+  console.log("Ok");
+  console.log(req.body);
+  users.push(req.body);
+  console.log(users);
+  res.send("Ok");
+});
+
+app.post("/tweets", (req, res) => {
+  console.log("Ok");
+  console.log(req.body);
+  tweets.push(req.body);
+  console.log(tweets);
+  res.send("Ok");
 });
 
 console.log("Rodando ... ");
