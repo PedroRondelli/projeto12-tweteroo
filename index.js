2e3d8fe75141e8bd3e194e66d626dc18b50b5f22
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-04 09:03:45 -0300

    finish backend setup

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
+app.use(express.json());
+app.use(cors());
 app.listen(5000);
 
-console.log("Rodando ... ")
+app.get("/here", (req, res) => {
+  res.send("Hello!!");
+});
+
+console.log("Rodando ... ");
