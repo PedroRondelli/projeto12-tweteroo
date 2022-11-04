4bcd73477e9a206554e0cd6cb01879feca8490f2
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-11-04 14:01:17 -0300

    respond to request with the last 10 tweets

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ import axios from "axios";
 import express from "express";
 import cors from "cors";
 
-const users = [];
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
 
-const tweets = [];
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o vasco",
+  },
+  {
+    username: "bobesponja",
+    tweet: "eu sou da GDA",
+  },
+  {
+    username: "bobesponja",
+    tweet: "o vasco é minha vida",
+  },
+];
 
 const app = express();
 app.use(express.json());
@@ -12,19 +31,41 @@ app.use(cors());
 app.listen(5000);
 
 app.post("/sign-up", (req, res) => {
-  console.log("Ok");
-  console.log(req.body);
   users.push(req.body);
-  console.log(users);
   res.send("Ok");
 });
 
 app.post("/tweets", (req, res) => {
-  console.log("Ok");
-  console.log(req.body);
   tweets.push(req.body);
-  console.log(tweets);
   res.send("Ok");
+});
+
+app.get("/tweets", (req, res) => {
+  const numberOfTweets = tweets.length;
+  const lastTweets = [];
+  if (numberOfTweets <= 10) {
+    for (let i = 0; numberOfTweets > i; i++) {
+      const { username, tweet } = tweets[i];
+      const tweetOwner = users.find((user) => user.username === username);
+      lastTweets.push({
+        username,
+        avatar: tweetOwner.avatar,
+        tweet,
+      });
+    }
+    return res.send(lastTweets);
+  } else {
+    for (let i = 0; 10 > i; i++) {
+      const { username, tweet } = tweets[i];
+      const tweetOwner = users.find((user) => user.username === username);
+      lastTweets.push({
+        username,
+        avatar: tweetOwner.avatar,
+        tweet,
+      });
+    }
+    return res.send(lastTweets);
+  }
 });
 
 console.log("Rodando ... ");
